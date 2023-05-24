a1b929fd560beca592bfe4b363178d603474f91c
Juan Manuel <jmgmgonzalez57@gmail.com>
2023-05-24 16:59:18 +0200

    Everything necessary to buils the library will be into the build.py file, may add a build_auto.py that will need administrative privileges, but be capable of managing absolutely all the dependencies. May discuss if this library has any users

---
+++ build.py
@@ -0,0 +1,10 @@
+#Automatically manage:
+#-> The dependencies in this order: (ninja wrapper for python) (build.ninja file cosntruction) (compilation with ninja)
+# (test compilation and run) (automated .obj file cleanup)
+#-> Make a useful info log about ninja and it's compile and clean options
+
+#Importds and other functions
+
+
+if __name__ == "__main__":
+    print("Build system ready")
